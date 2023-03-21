a949b90f54db6785c3f1eb0769b7f598ad7641e2
Albert <cautionsemen@gmail.com>
2023-03-21 20:20:06 +0300

    jquery get

--- SnailMS/Controllers/UserController.cs
+++ SnailMS/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 ﻿using CRUD;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SnailMS.Models;
 using System.Security.Claims;
 
 namespace SnailMS.Controllers
 {
+    [Authorize(Roles = "user")]
     public class UserController : Controller
     {
         private ILogger<UserController> logger;
@@ -22,7 +25,33 @@ namespace SnailMS.Controllers
         [HttpGet("/User/Account")]
         public IActionResult Account()
         {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                ViewBag.userId = HttpContext.User.Claims.ToList().Find(x => x.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+                ViewBag.role = HttpContext.User.Claims.ToList().Find(x => x.Type.Equals(ClaimTypes.Role)).Value;
+            }
             return View(service.Users.GetUserDtoById(HttpContext.User.Claims.ToList().Find(x => x.Type.Equals(ClaimTypes.NameIdentifier)).Value));
+        }
+        [HttpGet("/User/Users")]
+        public IActionResult SearchUser()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                ViewBag.userId = HttpContext.User.Claims.ToList().Find(x => x.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+                ViewBag.role = HttpContext.User.Claims.ToList().Find(x => x.Type.Equals(ClaimTypes.Role)).Value;
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetUsers(string name, string sortType, string filtType)// !access check
+        {
+            logger.LogInformation($"/GetUsers -> name:{name}");
+            List<UserDto> userDtos = service.Users.GetAllUserDto().ToList();
+            if (!string.IsNullOrEmpty(name))
+            {
+                userDtos = userDtos.Where(x => x.FirstName.Contains(name) || x.SecondName.Contains(name) || x.LastName.Contains(name)).ToList();
+            }
+            return PartialView(userDtos);
         }
 
     }
